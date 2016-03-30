816b82cd1fefc68a098f423c629943caedd998f5
eerto <eerto@naver.com>
2016-03-30 23:28:14 +0900

    pathname, method 추출

--- node/server.js
+++ node/server.js
@@ -2,13 +2,29 @@
  * Created by eerto_000 on 2016-03-27.
  */
 var http = require("http");
+var url = require("url");
 
 var serverObj;
 
-function setEvent(onRoute) {
+function setEvent(onRoute, handler) {
     serverObj.on("request", function(request, response) {
         console.log("[server] server.on request");
-        onRoute();
+        var pathName;
+        var method;
+        var urlObj;
+
+        console.log("[server] url : " + url.parse(request.url).href);
+
+        pathName = url.parse(request.url).pathname;
+        method = request.method;
+
+        urlObj = {
+            "pathName" : pathName,
+            "method" : method
+        };
+        
+        // pathName, method 에 따라 처리 
+        onRoute(urlObj, handler, response);
     });
 
     serverObj.on("connection", function(request, response) {
@@ -16,10 +32,11 @@ function setEvent(onRoute) {
     });
 }
 
-function _start(onRoute) {
-
+function _start(onRoute, handler) {
+    // 서버 생성
     serverObj = http.createServer();
-    setEvent(onRoute);
+    setEvent(onRoute, handler);
+    // 서버 실행
     serverObj.listen(8888);
 
     console.log("[server] Start server");
