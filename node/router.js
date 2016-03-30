c6b7b38e9e3c8da40f43b199e235c45bdca99a67
eerto <eerto@naver.com>
2016-03-30 23:27:27 +0900

    requestHandler 적용 - pathName, method 에 따라 처리 - 404 추가

--- node/router.js
+++ node/router.js
@@ -1,9 +1,18 @@
 /**
  * Created by eerto_000 on 2016-03-28.
  */
-function _router(pathName) {
-    console.log("[router] pathName : " + pathName);
+function _router(urlObj, handler, response) {
+    console.log("[router] pathName : " + urlObj.pathName);
 
+    if (typeof handler[urlObj.pathName] === "function") {
+        console.log("[router] Find");
+        // pathName 에 해당하는 콜백 수행
+        handler[urlObj.pathName](urlObj, response);
+    } else {
+        console.log("[router] Fail to find " + urlObj.pathName);
+        response.writeHead(404);
+        response.end();
+    }
 }
 
 exports.route = _router;
