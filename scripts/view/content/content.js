a0835d92934315888735235eb53eb3534c9c14ac
eerto <eerto@naver.com>
2016-06-03 00:12:08 +0900

    마우스 클릭 시 각 리스트 하위에 상세 항목 노출되도록 구현 - 상세항목으로 ul 추가 - ul 하위에 li 추가하여 상세리스트 화면 노출 구현 중...

--- scripts/view/content/content.js
+++ scripts/view/content/content.js
@@ -10,7 +10,7 @@ var Content = React.createClass({
                     this.props.printData.map(function(result, idx) {
                         var index = "";
 
-                        index = "list_" + idx;
+                        index = "list_" + idx + " ellipsis";
 
                         return <List className={index} key={idx} printData={result}/>;
                     })
@@ -20,12 +20,38 @@ var Content = React.createClass({
     }
 });
 
-var List = React.createClass( {
+var List = React.createClass({
+
+    handleClick : function(event) {
+        if (this.state.isOpen === true) {
+            this.setState({isOpen : false});
+        } else {
+            this.setState({isOpen : true});
+        }
+    },
+
     render : function() {
-        var importClass = this.props.className + " ellipsis";
-        
+        var importClassForDetail = "detail_" + this.props.key;
+        var listDetailTag;
+
+        if (this.state.isOpen === true) {
+            listDetailTag = <ListDetail className={importClassForDetail}/>
+        } else {
+            listDetailTag = "";
+        }
+
         return (
-          <li className={importClass}>{JSON.stringify(this.props.printData)}</li>
+          <li className={this.props.className} onClick={this.handleClick}>{JSON.stringify(this.props.printData)}</li>
+        );
+    }
+});
+
+var ListDetail = React.createClass({
+
+    render : function() {
+
+        return (
+            <ul className={this.props.className}></ul>
         );
     }
 });
